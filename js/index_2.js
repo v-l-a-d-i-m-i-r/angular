b99ac2d8a8b33bde0288a93922915c2ce5f33d2d
Vladimir <vladimir-dan88@yandex.ru>
2015-08-24 16:54:37 +0300

    Basic 24.08.2015

--- js/index_2.js
+++ js/index_2.js
@@ -1,26 +1,18 @@
+var persones = {
+    name: 'Vova',
+    age: 26,
+    profession: 'FE developer',
+    car: 'img/red_ferrari.jpg',
+    history: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt aliquid quod necessitatibus tempore laborum! Numquam quisquam, praesentium delectus quasi provident esse commodi nemo illum. Libero dolore quia placeat, ipsa voluptas non quaerat, veniam odio aperiam veritatis deserunt saepe commodi nam amet numquam! Cum incidunt quia blanditiis magnam fuga doloribus, aut.'
+}
+/*-------------------------------------------------*/
 var app = angular.module('demoApp', []);
 
 
-app.service("getObj",function($http){
-    return $http.get('http://127.0.0.1:1337/getObj');
-});
 
-app.controller('mainController', function($scope, getObj){
-
+app.controller('mainController', function($scope){
     $scope.main = {
-        init: function mainControllerInit(){
-            var self = this;
-
-            self.getData();
-        },
-        getData: function(){
-            var self = this;
-
-            getObj.then(function(data){
-                self.persones = data.data;
-            });
-        },
-        persones: {},
+        persones: persones,
         isShow: true,
         buttonLabel: 'Hide',
         toggleShow: function(isShow){
@@ -30,8 +22,6 @@ app.controller('mainController', function($scope, getObj){
             self.isShow = !isShow;
         }
     }
-
-    $scope.main.init();
 });
 
 
---
+++ js/index_3.js
@@ -0,0 +1,35 @@
+var app = angular.module('demoApp', []);
+
+
+app.service("getObj",function($http){
+    return $http.get('http://127.0.0.1:1337/getObj');
+});
+
+app.controller('mainController', function($scope, getObj){
+
+    $scope.main = {
+        init: function mainControllerInit(){
+            var self = this;
+
+            self.getData();
+        },
+        getData: function(){
+            var self = this;
+
+            getObj.then(function(data){
+                self.persones = data.data;
+            });
+        },
+        persones: {},
+        isShow: true,
+        buttonLabel: 'Hide',
+        toggleShow: function(isShow){
+            var self = this;
+
+            self.buttonLabel = isShow ? 'show' : 'hide';
+            self.isShow = !isShow;
+        }
+    }
+
+    $scope.main.init();
+});
